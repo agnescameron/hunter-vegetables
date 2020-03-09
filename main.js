8ae9d02e31e88deec3a206703bb0b1844660cbc9
agnescameron <agnesfcameron@gmail.com>
2020-03-09 12:34:07 -0400

    makin veg

---
+++ main.js
@@ -0,0 +1,8 @@
+// vegTimes.forEach( function(vegetable, index) {
+// 	console.log(vegetable.name)
+// })
+
+
+// vegTimes.forEach( function(vegetable, index) {
+// 	document.getElementById('vegbox').innerHTML += vegetable.name + '<br>'
+// })
